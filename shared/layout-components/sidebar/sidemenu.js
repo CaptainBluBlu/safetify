76ebba6e1a97bf42050e767020ddf60e1e744015
Lim Phang Zhen <limphangzhen@gmail.com>
2023-05-18 14:18:45 +0800

    Added options to side menus

--- shared/layout-components/sidebar/sidemenu.js
+++ shared/layout-components/sidebar/sidemenu.js
@@ -3,6 +3,14 @@ export const MENUITEMS = [
         menutitle: "MAIN",
         Items: [
             { path: `/components/dashboard/dashboard`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Dashboard' },
+            { path: `/components/settings/settings`, icon: 'fe fe-settings', type: 'link', active: false, selected: false, title: 'Settings' },
+            //create for blog postwith document icon
+            { path: `/components/blog/blogpost`, icon: 'fe fe-list', type: 'link', active: false, selected: false, title: 'Blog Post' },
+            // create for emergency housing with home icon
+            { path: `/components/housing/emergency-housing`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Emergency Housing' },
+            //create for file report with file icon
+            { path: `/components/report/file-report`, icon: 'fe fe-file', type: 'link', active: false, selected: false, title: 'File Report' },
+            
         ]
     },
     {
