12dffe27678c135844dae1529ae64d4a741d7c5f
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 12:58:29 +0800

    Added remove consolde for production in nextconfig and formatted

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  swcMinify: true,
-  basePath: isProd ? '/sash/preview' : undefined,
-  assetPrefix : isProd ? 'https://nextjs.spruko.com/sash/preview/' : undefined,
-  images: {
-    loader:"akamai",
-    path:""
-  },
-  compilerOptions: {
-    baseUrl: '.',
-  },
-}
+	reactStrictMode: true,
+	trailingSlash: true,
+	swcMinify: true,
+	basePath: isProd ? "/sash/preview" : undefined,
+	assetPrefix: isProd ? "https://nextjs.spruko.com/sash/preview/" : undefined,
+	images: {
+		loader: "akamai",
+		path: "",
+	},
+	compilerOptions: {
+		baseUrl: ".",
+		removeConsole: false,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
