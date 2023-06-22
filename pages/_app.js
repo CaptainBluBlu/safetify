e7d3c0b52e5dd97d6dbd2ad1408f42ca9caf7d1a
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 13:01:35 +0800

    Added some boilerplate for supabase, need to fix in future

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,34 @@
-import '@/styles/globals.scss'
-import Contentlayout from '../shared/layout-components/layout/content-layout'
-import Landingpagelayout from '../shared/layout-components/layout/landingpage-layout'
-import Switcherlayout from '../shared/layout-components/layout/switcher-layout'
-import Authenticationlayout from '../shared/layout-components/layout/authentication-layout'
-import { useEffect } from 'react'
+import "@/styles/globals.scss";
+import Contentlayout from "../shared/layout-components/layout/content-layout";
+import Landingpagelayout from "../shared/layout-components/layout/landingpage-layout";
+import Switcherlayout from "../shared/layout-components/layout/switcher-layout";
+import Authenticationlayout from "../shared/layout-components/layout/authentication-layout";
+
+import { createBrowserSupabaseClientClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 const layouts = {
-  Contentlayout: Contentlayout,
-  Landingpagelayout: Landingpagelayout,
-  Switcherlayout: Switcherlayout,
-  Authenticationlayout: Authenticationlayout,
+	Contentlayout: Contentlayout,
+	Landingpagelayout: Landingpagelayout,
+	Switcherlayout: Switcherlayout,
+	Authenticationlayout: Authenticationlayout,
 };
 export default function App({ Component, pageProps }) {
-  const Layout = layouts[Component.layout] || ((pageProps) => <Component>{pageProps}</Component>);
-  
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+	//const [supabaseClient] = useState(() => createBrowserSupabaseClientClient());
+
+	const Layout =
+		layouts[Component.layout] ||
+		((pageProps) => <Component>{pageProps}</Component>);
+
+	return (
+		// <SessionContextProvider
+		// 	supabaseClient={supabaseClient}
+		// 	initialSession={pageProps.initialSession}
+		// >
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+		// </SessionContextProvider>
+	);
 }
