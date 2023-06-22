95555c212ffaea39410ea2d25652d15973e16ae1
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 13:02:36 +0800

    Minor adjustments

--- pages/components/chat/index.js
+++ pages/components/chat/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Seo from '@/shared/layout-components/seo/seo';
 import dynamic from 'next/dynamic';
-const ChatPage = dynamic(() => import("../../../shared/data/chat/chat"), {ssr: false,});
+const ChatPage = dynamic(() => import("../../../shared/data/chat/chatComponent"), {ssr: false,});
 
 const index = () => {
   return (
--- pages/components/dashboard/dashboard.js
+++ pages/components/dashboard/dashboard.js
@@ -2,15 +2,22 @@ import Seo from "@/shared/layout-components/seo/seo";
 import dynamic from "next/dynamic";
 import React from "react";
 const Dashboard = dynamic(
-	() => import("../../../shared/data/datadashboard/dashbord"),
+	() => import("../../../shared/data/datadashboard/authoritiesDashboard"),
 	{ ssr: false }
 );
 
 const DashboardCom = () => {
 	return (
 		<div>
-			<Seo title="Dashboard" />
-
+			{/* 
+			 if user:
+			 	user dashboard
+			 else if authorities:
+			 	authorities dashboard
+			 else if volunteer:
+			 	volunteer dashboard
+			> */}
+			<Seo title="Safetify" />
 			<Dashboard />
 		</div>
 	);
