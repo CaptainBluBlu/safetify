dd56d6cce28af6048d30df915c4952c82229cf02
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 13:05:43 +0800

    Commented emergency panic button file

--- pages/components/emergency-panic-button/emergency-panic-button.js
+++ pages/components/emergency-panic-button/emergency-panic-button.js
@@ -1,37 +1,40 @@
-//create emeergency housing page based on the other template files
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-import Seo from '@/shared/layout-components/seo/seo';
-const Dashboard = dynamic(() => import("../../../shared/data/datadashboard/dashbord"), {ssr: false,});
+// //create emeergency housing page based on the other template files
+// import React, { useState } from "react";
+// import dynamic from "next/dynamic";
+// import Seo from "@/shared/layout-components/seo/seo";
+// const Dashboard = dynamic(
+// 	() => import("../../../shared/data/datadashboard/dashbord"),
+// 	{ ssr: false }
+// );
 
-const EmergencyHousing = () => {
-    return (
-        <div>
-            <Seo title="Emergency Panic Button"/>
-            <EmergencyPanicButton/>
-        </div>
-    )
-    }
+// const EmergencyHousing = () => {
+// 	return (
+// 		<div>
+// 			<Seo title="Emergency Panic Button" />
+// 			{/* <EmergencyPanicButton/> */}
+// 		</div>
+// 	);
+// };
 
-function App() {
-  const [isEmergency, setIsEmergency] = useState(false);
+// function App() {
+// 	const [isEmergency, setIsEmergency] = useState(false);
 
-  const handlePanicButton = () => {
-    setIsEmergency(true);
+// 	const handlePanicButton = () => {
+// 		setIsEmergency(true);
 
-    // Send emergency signal or perform any necessary actions here
-    // You can replace the alert with appropriate code for your application
-    alert('Emergency signal sent!');
-  };
+// 		// Send emergency signal or perform any necessary actions here
+// 		// You can replace the alert with appropriate code for your application
+// 		alert("Emergency signal sent!");
+// 	};
 
-  return (
-    <div className="App">
-      <h1>Emergency Panic Button</h1>
-      <button onClick={handlePanicButton}>Panic Button</button>
-      {isEmergency && <p>Emergency signal sent!</p>}
-    </div>
-  );
-}
+// 	return (
+// 		<div className="App">
+// 			<h1>Emergency Panic Button</h1>
+// 			<button onClick={handlePanicButton}>Panic Button</button>
+// 			{isEmergency && <p>Emergency signal sent!</p>}
+// 		</div>
+// 	);
+// }
 
-EmergencyPanicButton.layout = "Contentlayout"
-export default EmergencyPanicButton;
+// EmergencyPanicButton.layout = "Contentlayout";
+// export default EmergencyPanicButton;
