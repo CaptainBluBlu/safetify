f30d08c48d1b0001097ca72ae96722e5802f0523
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 13:04:56 +0800

    Added some files

---
+++ shared/layout-components/panicButton.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+import { useState, useEffect } from "react";
+
+const panicButton = () => {
+	const buttonStyling = {
+		height: "400px",
+		width: "400px",
+	};
+
+	// Handles the responsive design for the page
+	const [isMd, setIsMd] = useState(window.innerWidth >= 768);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMd(window.innerWidth >= 768);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	if (window.innerWidth <= 768) {
+		buttonStyling.height = "350px";
+		buttonStyling.width = "350px";
+	}
+
+	if (window.innerWidth <= 600) {
+		buttonStyling.height = "300px";
+		buttonStyling.width = "300px";
+	}
+
+	return (
+		<>
+			<Button
+				variant="danger"
+				style={buttonStyling}
+				className="rounded-circle fixed-sized-button"
+			>
+				SOS
+			</Button>
+		</>
+	);
+};
+
+export default panicButton;
