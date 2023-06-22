8b9ef6309cbc0c25fa9c6ec0e880d4a3cd6623fd
Lim Phang Zhen <limphangzhen@gmail.com>
2023-06-22 13:05:15 +0800

    updated justice board ui

--- pages/components/justiceboard/index.js
+++ pages/components/justiceboard/index.js
@@ -6,12 +6,37 @@ const JusticeBoard = dynamic(
 	{ ssr: false }
 );
 
-const JusticeBoardCom = () => {
+export async function getStaticProps() {
+	try {
+		const client = require("contentful").createClient({
+			space: "84vvet7b87tt",
+			accessToken: "vlOOkzU2IxnPMQqBkL74mfvf-34UXLdKSX6syvgyesk",
+		});
+
+		const res = await client.getEntries({ content_type: "justicePost" });
+
+		return {
+			props: {
+				blogPost: res.items,
+			},
+		};
+	} catch {
+		console.log("Error fetching data from Contentful:", error);
+		return {
+			props: {
+				blogPost: [],
+			},
+		};
+	}
+}
+
+const JusticeBoardCom = ({ blogPost }) => {
+	console.log(blogPost);
 	return (
 		<div>
-			<Seo title="Dashboard" />
+			<Seo title="Justice Board - Safetify" />
 
-			<JusticeBoard />
+			<JusticeBoard blogPost={blogPost} />
 		</div>
 	);
 };
--- shared/data/justiceboard/justiceBoardMain.js
+++ shared/data/justiceboard/justiceBoardMain.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Container, Card, Button } from "react-bootstrap";
 
-const BulletinBoard = () => {
+const BulletinBoard = ({ blogPost }) => {
+	console.log(blogPost);
 	const articles = [
 		{
 			id: 1,
@@ -15,24 +16,47 @@ const BulletinBoard = () => {
 		// Add more articles here
 	];
 
+	const justicePost = [
+		{
+			id: 1,
+			title: "Exciting News!",
+		},
+	];
 	return (
 		<Container>
 			<h1>Bulletin Board</h1>
-			{articles.map((article) => (
-				<Card key={article.id} className="mb-4">
-					<Card.Img variant="top" src={article.image} />
+			{blogPost.map((article) => (
+				<Card key={article.sys.id} className="mb-4">
+					<Card.Img variant="top" src={article.fields.thumbnail} />
 					<Card.Body>
-						<Card.Title>{article.title}</Card.Title>
+						<Card.Title>{article.fields.title}</Card.Title>
 						<Card.Subtitle className="mb-2 text-muted">
-							{article.author} | {article.datePosted}
+							{article.fields.author} | {article.fields.datePosted}
 						</Card.Subtitle>
-						<Card.Text>{article.summary}</Card.Text>
-						<Button variant="primary" href={`/article/${article.id}`}>
+						<Card.Text>{article.fields.summary}</Card.Text>
+						<Button variant="primary" href={`/article/${article.fields.id}`}>
 							Read More
 						</Button>
 					</Card.Body>
 				</Card>
 			))}
+			{/* <div>
+				{justicePost.map((post) => (
+					<Card key={blogPost.sys.id} className="mb-4">
+						<Card.Img variant="top" src={blogPost.fields.thumbnail} />
+						<Card.Body>
+							<Card.Title>{blogPost.fields.title}</Card.Title>
+							<Card.Subtitle className="mb-2 text-muted">
+								{blogPost.fields.author} | {blogPost.fields.uploadDate}
+							</Card.Subtitle>
+							<Card.Text>{blogPost.fields.summary}</Card.Text>
+							<Button variant="primary" href={`/article/${article.id}`}>
+								Read More
+							</Button>
+						</Card.Body>
+					</Card>
+				))}
+			</div> */}
 		</Container>
 	);
 };
